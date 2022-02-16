46154cb01670ce10eac536b1f2c86dfe97ee9380
Zeke Marffy <zmarffy@me.com>
2022-02-15 20:23:05 -0500

    allow missing version

--- zetuptools/__init__.py
+++ zetuptools/__init__.py
@@ -4,4 +4,7 @@ package_name = "zetuptools"
 
 __version__ = "0.0.0"
 if __version__ == "0.0.0":
-    __version__ = PipPackage(package_name).version
+    try:
+        __version__ = PipPackage(package_name).version
+    except FileNotFoundError:
+        pass
